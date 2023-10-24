ffd85e2ea10fc0f5be4de731c9f64ffe8d3aa3df
Vigdis-Irene Steinsund <vigdis.steinsund@hotmail.com>
2023-10-24 13:51:19 -0300

    🐸✨Finish task 7, 8 & 10

--- exercise_3/app.py
+++ exercise_3/app.py
@@ -1,10 +1,7 @@
 import datetime
 from DbConnector import DbConnector
 from pprint import pprint 
-import statistics
-
-
-
+from haversine import haversine
 
 class App:
 
@@ -139,18 +136,144 @@ class App:
     def task_7(self):
         print("Task 7")
 
+        activities = self.db["activities"]
+        result = list(activities.aggregate([
+            # Fetch walk activities and corresponding trackpoints for user 112 
+            # performed in year 2008
+            {"$match": {
+                "user_id": 112,
+                "transportation_mode": "walk",
+                'start_date_time': {'$regex':'^2008'}
+            }},
+            {"$lookup": {
+                "from": "trackpoints",
+                "localField": "_id",
+                "foreignField": "activity_id",
+                "as": "trackpoints"
+            }}
+        ]))
+
+        # Calculate total distance for each trackpoint in each activity
+        total_distance = 0
+        for activity in result:
+            trackpoints = activity['trackpoints']
+            for i in range(len(trackpoints) - 1):
+                # Use haversine formula to calculate distance between the 
+                # current trackpoint and the next one, and add this to the 
+                # total distance
+                total_distance += haversine((float(trackpoints[i]['lat']), float(trackpoints[i]['lon'])), (float(trackpoints[i+1]['lat']), float(trackpoints[i+1]['lon'])))
+
+        print(f'Total distance: {total_distance} km')
         print("-----------------------")
     
     def task_8(self):
         print("Task 8")
+
+        # Get all valid trackpoints sorted by activity_id and timestamp
+        trackpoints = list(self.db['trackpoints'].find({"altitude": {"$ne": '-777'}}).sort([('activity_id', 1), ('timestamp', 1)]))
+
+        # Initialize variables for calculating altitude gains per activity
+        prev_trackpoint = None
+        altitude_gains = {}
+
+        # Calculate altitude gains per activity
+        for trackpoint in trackpoints:
+            # Check if the current trackpoint is not the first one recorded for
+            # and activity and that it is part of the same activity as the 
+            # previous trackpoint, i.e. it has the same activity_id
+            if prev_trackpoint is not None and trackpoint['activity_id'] == prev_trackpoint['activity_id']:
+                # Calculate altitude difference between the current trackpoint
+                # and the previous one
+                altitude_diff = float(trackpoint['altitude']) - float(prev_trackpoint['altitude'])
+                # Check if the altitude difference is positive, i.e. the current
+                # trackpoint is at a higher altitude than the previous one
+                if altitude_diff > 0:
+                    activity_id = trackpoint['activity_id']
+                    # Check if the current activity has already been added to 
+                    # the altitude_gains dictionary, if not add it
+                    if activity_id not in altitude_gains:
+                        altitude_gains[activity_id] = 0
+                    # Add the altitude difference to the total altitude gain
+                    # for the current activity 
+                    altitude_gains[activity_id] += altitude_diff
+            # Update the previous trackpoint to be the current one, to check
+            # for altitude differences in the next iteration
+            prev_trackpoint = trackpoint
+
+        # Convert altitude_gains dictionary to list of documents to be inserted
+        # as a temporary collection to our database
+        docs = [{'activity_id': k, 'altitude_gain': v} for k, v in altitude_gains.items()]
+
+        # Insert the documents into the new altitude_gains collection
+        self.db['altitude_gains'].insert_many(docs)
+
+        alt_gain_per_activity = self.db['altitude_gains']
+        result = list(alt_gain_per_activity.aggregate([
+            # Join activities and altitude_gains collections on activity_id
+            # to extract user_id for each activity, since we want to sum the 
+            # total altitude gains across all activities for each user
+            {"$lookup": {
+                "from": "activities",
+                "localField": "activity_id",
+                "foreignField": "_id",
+                "as": "activity"
+            }},
+            {"$group": {
+                "_id": "$activity.user_id",
+                "total_meters_gained_per_user": {
+                    "$sum": {
+                        "$multiply": ["$altitude_gain", 0.3048]  # Convert feet to meters
+                    }
+                }
+            }},
+            {"$sort": {"total_meters_gained_per_user": -1}},
+            {"$limit": 20}
+        ]))
+
+        pprint(result)
+
+        # Drop the temporary altitude_gains collection
+        self.db['altitude_gains'].drop()
+
         print("-----------------------")
     
     def task_9(self):
         print("Task 9")
+        # TODO: Implement this
         print("-----------------------")
     
     def task_10(self):
         print("Task 10")
+
+        trackpoints = self.db["trackpoints"]
+        result = list(trackpoints.aggregate([
+            # Fetch all trackpoints that start with latitude 39.916 and 
+            # longitude 116.397
+            {"$match": {
+                'lat': {'$regex':'^39.916'},
+                'lon': {'$regex':'^116.397'}
+            }},
+            # Join trackpoints and activities collections on activity_id to 
+            # extract the user_id for each activity, since we want to count
+            # the number of unique users that have performed activities in
+            # the Forbidden City of Beijing
+            {"$lookup": {
+                "from": "activities",
+                "localField": "activity_id",
+                "foreignField": "_id",
+                "as": "activity_info"
+            }},
+            # Unwind the activity_info array to get a document for each 
+            # activity
+            {"$unwind": "$activity_info"},
+            # Group by user_id to get all unique users that have performed
+            # activities in this area
+            {"$group": {
+                "_id": "$activity_info.user_id"
+            }}
+        ]))
+
+        pprint(result)
     
         print("-----------------------")
 
@@ -202,7 +325,7 @@ def main():
         program.task_6()
         program.task_7()
         program.task_8()
-        program.task_9()
+        # program.task_9()
         program.task_10()
         program.task_11()
     except Exception as e:
